92406dd48100ecdd161d026290dbfdfa013fa5b4
McECDQEMSD <alejandroortizmc@icloud.com>
2021-11-12 00:17:29 -0400

    class 5 score and menu
    correccion de errores de en clase y completado de codigo

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -13,15 +13,16 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-        self.game_speed = 20
+        self.game_speed = 30
         self.clock = pygame.time.Clock()
         self.player = Dinosaur()
-        self.obstacle = ObstacleManager()
+        self.obstacle_manager = ObstacleManager()
         self.points = 0
+        self.death_count = 0
 
     def score(self):
         self.points += 1
-        if self.points % 20 == 0:
+        if self.points % 10 == 0:
             self.game_speed += 1
         score_element, score_element_rec = text_utils.get_score_element(self.points, )
         self.screen.blit(score_element, score_element_rec)
@@ -38,6 +39,8 @@ class Game:
         half_heigth = SCREEN_HEIGHT //2
         text_element, text_element_rec = text_utils.get_centered_message('Press any key to start', )
         self.screen.blit(text_element, text_element_rec)
+        text_element, text_element_rec = text_utils.get_centered_message('Death Count: ' + str(self.death_count), height = half_heigth + 50)
+        self.screen.blit(text_element, text_element_rec)
         self.screen.blit(ICON, (half_width - 40, half_heigth -150))
 
     def handle_key_events_on_menu(self):
@@ -52,6 +55,8 @@ class Game:
                 self.run()
 
     def run(self):
+        self.points = 0
+        self.obstacle_manager.reset_obstacles()
         self.playing = True
         while self.playing:
             self.events()
@@ -74,7 +79,7 @@ class Game:
     def update(self):
         user_imput = pygame.key.get_pressed()
         self.player.update(user_imput)
-        self.obstacle.update(self)
+        self.obstacle_manager.update(self)
 
 
     def draw(self):
@@ -82,7 +87,7 @@ class Game:
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
-        self.obstacle.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         self.score()
         pygame.display.update()
         pygame.display.flip()
--- nlc_dino_runner/utils/text_utils.py
+++ nlc_dino_runner/utils/text_utils.py
@@ -11,9 +11,9 @@ def get_score_element(points):
     text_rect.center = (1000, 25)
     return text, text_rect
 
-def get_centered_message(message):
+def get_centered_message(message, width = SCREEN_WIDTH // 2 , height = SCREEN_HEIGHT // 2):
     font = pygame.font.Font(FONT_STALY, 30)
     text = font.render(message, True, BLACK_COLOR)
     text_rect = text.get_rect()
-    text_rect.center = (SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2)
+    text_rect.center = (width, height)
     return text, text_rect
